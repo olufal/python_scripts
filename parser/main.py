fb946c6a099543db507e768616cfa26580dd1446
olufal <42657176+olufal@users.noreply.github.com>
2020-06-12 07:32:21 -0600

    added blob storage logic

--- parser/main.py
+++ parser/main.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from strive.note import CAMNotes
+from strive.note_parser import CAMNotes
 from azure.storage.blob import BlockBlobService
 from datetime import datetime
-import dask.dataframe as dd
 
 # Define parameters
 storageAccountName = "devinternalanalyticssa"
 storageKey         = "V0asRZHya4Gutd4jQpmBvtw4l9bDKL5CiJs3c4d7qTtn0AJ60JiVQT4imW/hFNXRQSEYvK+9mKWme+jpIWC5mg=="
 containerName      = "blobstage"
-
+blobDirectory      = "bullhorn/python"
 
 # Establish connection with the blob storage account
 blobService = BlockBlobService(account_name=storageAccountName,
@@ -17,26 +16,17 @@ blobService = BlockBlobService(account_name=storageAccountName,
 batchAccount = 'deviabatch'
 batchURL = 'https://deviabatch.eastus.batch.azure.com'
 primaryAccessKey = '/MTRr7/uq9BfvO8mGVUMB1QhrymlfntGVyp+1z/idSitCJ6265xAjMlqV8V+axrQeblhlLRVlSLiPMqDpIm/zQ=='
-blobDirectory = 'bullhorn/python'
-
-def etl(input_file):
-    raw_data = pd.read_csv(input_file)
-    df = raw_data['comments']
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    output_file = f'parsed_note_{now}.csv'
-    df.to_csv(output_file)
-    blobService.create_blob_from_text(containerName, output_file, output_file)
 
 
-def main(extract_file: str):
-    raw_data = pd.read_csv(extract_file)
+def parser(input_file: str):
+    raw_data = pd.read_csv(input_file, sep='\t')
     parsed_df = pd.DataFrame()
 
     for i, row in raw_data.iterrows():
         note = CAMNotes(note_id=row['noteid'],
                         comments=row['comments'],
                         person_id=row['commentingpersonid'],
-                        create_datetime=row['dateadded'],
+                        cam_datetime=row['dateadded'],
                         action=row['action'])
         note.generate()
         parsed_df = parsed_df.append(note.note_df, ignore_index=True, sort=False)
@@ -45,13 +35,13 @@ def main(extract_file: str):
     parsed_file = f'parsed_note_{now}.csv'
     parsed_df.to_csv(parsed_file, index=False)
 
-    # Upload iris dataset
-    blobService.create_blob_from_text(containerName, parsed_file, parsed_file)
+    # Upload dataset to blobstage
+    blobService.create_blob_from_path(containerName, f'{blobDirectory}/{parsed_file}', parsed_file)
+
+    return parsed_df
+
 
 
 if __name__ == '__main__':
-    # file_path = r'C:\out\ClientMeetingNotes_test.csv'
-    # df = pd.read_csv(file_path, sep='\t')
-    # main(file_path)
-    input_file = 'ClientMeetingNotes.csv'
-    etl(input_file)
+    input_file = r'C:\out\ClientMeetingNotes_test.csv'
+    df = parser(input_file)
